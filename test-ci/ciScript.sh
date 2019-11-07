09f2349fc4fe149f2e7e2b7c3e17adfb0031b26f
Jessica Colson <jessica.colson@torrentconsulting.com>
2019-11-07 09:18:25 -0500

    Updated ci script

--- test-ci/ciScript.sh
+++ test-ci/ciScript.sh
@@ -4,6 +4,12 @@ grep "^test-ci\/force-app" temp.txt > files.txt
 awk '{gsub(/test-ci\//,"")}1' files.txt > temp.txt && mv temp.txt files.txt
 head files.txt
 # need to remove file names that aren't in the force-app directory
-export FILE_LIST="$(awk '{print}' ORS=',' files.txt | sed '$s/.$//')"
-echo $FILE_LIST
-/usr/local/bin/sfdx force:source:convert -d mdapi-out -p $FILE_LIST
+FILE_LIST="$(awk '{print}' ORS=',' files.txt | sed '$s/.$//')"
+if [ $FILE_LIST ]
+then
+    echo $FILE_LIST
+    /usr/local/bin/sfdx force:source:convert -d mdapi-out -p $FILE_LIST
+else
+    echo "No files have changed since last successful build."
+fi
+
