544cb9f5c60696fda6f1aed5a491f105dd43193b
Jessica Colson <jessica.colson@torrentconsulting.com>
2019-11-06 22:20:38 -0500

    Updated ci script

--- test-ci/ciScript.sh
+++ test-ci/ciScript.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-git diff --name-only $1 $2 > files.txt
-grep "^test-ci\/force-app" files.txt > files.txt
+git diff --name-only $1 $2 > temp.txt
+grep "^test-ci\/force-app" temp.txt > files.txt
 # need to remove file names that aren't in the force-app directory
 FILE_LIST=$(awk '{print}' ORS=', ' files.txt | sed '$s/..$//')
 echo $FILE_LIST
