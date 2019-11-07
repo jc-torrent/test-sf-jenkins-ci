98f81d087f1931325ef46b66f5098d8b8a920277
Jessica Colson <jessica.colson@torrentconsulting.com>
2019-11-06 22:19:27 -0500

    Updated ci script

--- test-ci/ciScript.sh
+++ test-ci/ciScript.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 git diff --name-only $1 $2 > files.txt
+grep "^test-ci\/force-app" files.txt > files.txt
+# need to remove file names that aren't in the force-app directory
 FILE_LIST=$(awk '{print}' ORS=', ' files.txt | sed '$s/..$//')
 echo $FILE_LIST
 /usr/local/bin/sfdx force:source:convert -d mdapi-out -p "$FILE_LIST"
